c54b0e62f07005294dc90df2e444e22bd637df09
Michael Tsang <michael@itwakes.me>
2017-04-03 18:09:14 +0800

    add URL generation for WebDAV adapter

--- src/WebDAVAdapter.php
+++ src/WebDAVAdapter.php
@@ -44,6 +44,11 @@ class WebDAVAdapter extends AbstractAdapter
     protected $useStreamedCopy = true;
 
     /**
+     * @var string|null;
+     */
+    private $urlPrefix = null;
+
+    /**
      * Constructor.
      *
      * @param Client $client
@@ -380,5 +385,41 @@ class WebDAVAdapter extends AbstractAdapter
         $result['path'] = trim($path, '/');
 
         return $result;
+    }
+
+    /**
+     * Get the URL for the file at the given path.
+     *
+     * @param  string  $path
+     * @return string
+     */
+    public function getUrl($path)
+    {
+        $prefix = $this->getUrlPrefix();
+        return $prefix !== null
+            ? rtrim($prefix, '/') . '/' . ltrim($path, '/')
+            : $this->client->getAbsoluteUrl($path);
+    }
+
+    /**
+     * Get the custom URL prefix for URL generation
+     *
+     * @return string|null
+     */
+    public function getUrlPrefix()
+    {
+        return $this->urlPrefix;
+    }
+
+    /**
+     * Set the custom URL prefix for URL generation.
+     *
+     * If the prefix is not set, the baseUri of DAV client is used.
+     *
+     * @param string|null $prefix
+     */
+    public function setUrlPrefix($prefix)
+    {
+        $this->urlPrefix = $prefix;
     }
 }
