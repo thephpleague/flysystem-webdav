bfbd39983edc6fa147f65a7227a1f0883704d821
Frank de Jonge <info@frankdejonge.nl>
2025-01-26 14:57:48 +0100

    Default to empty string

--- ByteMarkWebDAVServerTest.php
+++ ByteMarkWebDAVServerTest.php
@@ -10,7 +10,7 @@ class ByteMarkWebDAVServerTest extends WebDAVAdapterTestCase
 {
     protected static function createFilesystemAdapter(): FilesystemAdapter
     {
-        if ($_ENV['TEST_WEBDAV'] !== 'YES') {
+        if (($_ENV['TEST_WEBDAV'] ?? '') !== 'YES') {
             self::markTestSkipped('Library regression');
         }
         $client = new UrlPrefixingClientStub(['baseUri' => 'http://localhost:4080/', 'userName' => 'alice', 'password' => 'secret1234']);
